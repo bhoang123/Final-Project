9ba233950464603d91deb1bc8e43c59b1e4a8b49
bhoang <baobinhbuffon@gmail.com>
2022-12-04 01:27:00 -0500

    final project

---
+++ src/data/Products.js
@@ -0,0 +1,70 @@
+const products = [
+  {
+    _id: "1",
+    name: " 1984 by George Orwell",
+    image: "/images/1.png",
+    description:
+      "1984 tells the futuristic story of a dystopian, totalitarian world where free will and love are forbidden. Although the year 1984 has long since passed, the prophecy of a society controlled by fear and lies is arguably more relevant now than ever.",
+    price: 14,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 38,
+  },
+  {
+    _id: "2",
+    name: "The Lord of the Rings by J.R.R. Tolkien",
+    image: "/images/2.png",
+    description:
+      "Tolkiens fantasy epic is one of the top must-read books out there. Set in Middle Earth a world full of hobbits, elves, orcs, goblins, and wizards – The Lord of the Rings will take you on an unbelievable adventure",
+    price: 49,
+    countInStock: 7,
+    rating: 2,
+    numReviews: 2,
+  },
+  {
+    _id: "3",
+    name: "The Kite Runner by Khaled Hosseini",
+    image: "/images/3.png",
+    description:
+      "The Kite Runner is a moving story of an unlikely friendship between a wealthy boy and the son of his father’s servant. Set in Afghanistan during a time of tragedy and destruction, this unforgettable novel will have you hooked from start to finish",
+    price: 399,
+    countInStock: 10,
+    rating: 5,
+    numReviews: 9,
+  },
+  {
+    _id: "4",
+    name: "Harry Potter and the Philosophers Stone by J.K. Rowling",
+    image: "/images/4.png",
+    description:
+      "This global bestseller took the world by storm. So, if you havent read J.K. Rowlings Harry Potter, now may be the time. Join Harry Potter and his schoolmates as this must-read book transports you deep into a world of magic and monsters",
+    price: 929,
+    countInStock: 0,
+    rating: 3.5,
+    numReviews: 3,
+  },
+  {
+    _id: "5",
+    name: "Slaughterhouse-Five by Kurt Vonnegut",
+    image: "/images/5.png",
+    description:
+      "Slaughterhouse-Five is arguably one of the greatest anti-war books ever written. This rich and amusing tale follows the life of Billy Pilgrim as he experiences World War II from a peculiar perspective",
+    price: 599,
+    countInStock: 10,
+    rating: 2,
+    numReviews: 2
+  },
+  {
+    _id: "6",
+    name: " 1984 by George Orwell",
+    image: "/images/1.png",
+    description:
+      "1984 tells the futuristic story of a dystopian, totalitarian world where free will and love are forbidden. Although the year 1984 has long since passed, the prophecy of a society controlled by fear and lies is arguably more relevant now than ever.",
+    price: 14,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 38,
+  },
+];
+
+export default products;
